101ecc744a845b43f4c03e0041b3566b275784ef
Dirk <dg.schudde@live.nl>
2024-12-03 11:09:45 +0100

    day three, part two

--- day-three/day-three.go
+++ day-three/day-three.go
@@ -30,18 +30,30 @@ func calculateTotal(instructions []string) int64 {
 	var regex = regexp.MustCompile(`\d+`)
 
 	var total int64
+	var multiplyEnabled = true
 
 	for _, instruction := range instructions {
-		numbers := regex.FindAllString(instruction, -1)
-		left, _ := strconv.ParseInt(numbers[0], 10, 64)
-		right, _ := strconv.ParseInt(numbers[1], 10, 64)
-		total = total + left*right
+		if instruction == `don't()` {
+			multiplyEnabled = false
+			continue
+		}
+		if instruction == `do()` {
+			multiplyEnabled = true
+			continue
+		}
+
+		if multiplyEnabled {
+			numbers := regex.FindAllString(instruction, -1)
+			left, _ := strconv.ParseInt(numbers[0], 10, 64)
+			right, _ := strconv.ParseInt(numbers[1], 10, 64)
+			total = total + left*right
+		}
 	}
 	return total
 }
 
 func matchInstructions(input string) []string {
-	regex := regexp.MustCompile(`mul\(\d+,\d+\)`)
+	regex := regexp.MustCompile(`mul\(\d+,\d+\)|don't\(\)|do\(\)`)
 	matches := regex.FindAllString(input, -1)
 	return matches
 }
